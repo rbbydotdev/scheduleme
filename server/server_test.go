0dcb2abb005ec7556b026efabd0cae0ea55e1fb5
Robert Polana <robertapolana@gmail.com>
2024-09-11 11:47:35 +0700

    fix: server run main test from hanging

--- server/server_test.go
+++ server/server_test.go
@@ -7,11 +7,11 @@ import (
 
 // TestRunMain tests the RunMain function
 
-func TestRunMain(t *testing.T) {
+func TestRun(t *testing.T) {
 	defer func() {
 		if r := recover(); r != nil {
-			t.Errorf("RunMain panicked with: %v", r)
+			t.Errorf("Run panicked with: %v", r)
 		}
 	}()
-	server.RunMain()
+	server.Run(nil, nil)
 }
